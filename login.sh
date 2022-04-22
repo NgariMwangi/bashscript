950cf02a3c0416b89a7d131692323c9ef0739570
denzoh4 <dennohmwangi4@gmail.com>
2022-04-23 02:50:37 +0300

    shell script run trial 26

--- login.sh
+++ login.sh
@@ -1,2 +1,2 @@
-ssh root@138.68.189.32 "git clone https://github.com/denzoh4/bashscript.git; cd bashscript; docker-compose down; docker-compose up --build -d"
+ssh root@138.68.189.32 " git pull; cd bashscript; docker-compose down; docker-compose up --build -d"
 
